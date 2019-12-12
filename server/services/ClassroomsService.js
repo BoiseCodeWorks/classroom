dadadf4c7e60586bf68e7c2aae27b772eb796063
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2019-12-12 11:02:07 -0700

    Daniel

---
+++ server/services/ClassroomsService.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import Classroom from "../models/Classroom";
+import ApiError from "../utils/ApiError";
+
+const _repository = mongoose.model("Classroom", Classroom);
+
+class ClassroomService {
+  async getAll() {
+    return await _repository.find({});
+  }
+  async getById(id) {
+    return await _repository.findById(id);
+  }
+  async create(rawData) {
+    return await _repository.create(rawData);
+  }
+  async edit(id, update) {
+    let data = await _repository.findOneAndUpdate({ _id: id }, update, {
+      new: true
+    });
+    if (!data) {
+      throw new ApiError("Invalid Update ID", 400);
+    }
+    return data;
+  }
+  async delete(id) {
+    let data = await _repository.findOneAndRemove({ _id: id });
+    if (!data) {
+      throw new ApiError("Invalid Update ID", 400);
+    }
+  }
+}
+
+const classroomService = new ClassroomService();
+export default classroomService;
